bdd19caf71b4eff1024423f33e8982beb96c8e22
MuhammadArslan9300 <muhammadarslan9300@gmail.com>
2025-10-16 10:52:12 +0500

    cms connected

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -17,6 +17,7 @@ import endpoints from './endpoints'
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
+
 export default buildConfig({
   cors: [
     'http://localhost:3000', // Next.js frontend
