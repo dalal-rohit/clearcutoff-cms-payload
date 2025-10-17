82dce846cc9dd6bc9166df1ff062bba6da978bf8
MuhammadArslan9300 <muhammadarslan9300@gmail.com>
2025-10-17 11:37:33 +0500

    cms connected

--- src/payload-migrations/index.ts
+++ src/payload-migrations/index.ts
@@ -1,57 +1,15 @@
-import * as migration_20251017_034215 from './20251017_034215';
-import * as migration_20251017_035000 from './20251017_035000';
-import * as migration_20251017_035538 from './20251017_035538';
-import * as migration_20251017_040216 from './20251017_040216';
-import * as migration_20251017_045940 from './20251017_045940';
-import * as migration_20251017_050513 from './20251017_050513';
-import * as migration_20251017_051035 from './20251017_051035';
-import * as migration_20251017_051701 from './20251017_051701';
 import * as migration_20251017_062920 from './20251017_062920';
+import * as migration_20251017_063721 from './20251017_063721';
 
 export const migrations = [
   {
-    up: migration_20251017_034215.up,
-    down: migration_20251017_034215.down,
-    name: '20251017_034215',
-  },
-  {
-    up: migration_20251017_035000.up,
-    down: migration_20251017_035000.down,
-    name: '20251017_035000',
-  },
-  {
-    up: migration_20251017_035538.up,
-    down: migration_20251017_035538.down,
-    name: '20251017_035538',
-  },
-  {
-    up: migration_20251017_040216.up,
-    down: migration_20251017_040216.down,
-    name: '20251017_040216',
-  },
-  {
-    up: migration_20251017_045940.up,
-    down: migration_20251017_045940.down,
-    name: '20251017_045940',
-  },
-  {
-    up: migration_20251017_050513.up,
-    down: migration_20251017_050513.down,
-    name: '20251017_050513',
-  },
-  {
-    up: migration_20251017_051035.up,
-    down: migration_20251017_051035.down,
-    name: '20251017_051035',
-  },
-  {
-    up: migration_20251017_051701.up,
-    down: migration_20251017_051701.down,
-    name: '20251017_051701',
-  },
-  {
     up: migration_20251017_062920.up,
     down: migration_20251017_062920.down,
-    name: '20251017_062920'
+    name: '20251017_062920',
+  },
+  {
+    up: migration_20251017_063721.up,
+    down: migration_20251017_063721.down,
+    name: '20251017_063721'
   },
 ];
