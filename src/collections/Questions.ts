cfff0c14f6af60ce28171bf155833522bd01ed8e
MuhammadArslan9300 <muhammadarslan9300@gmail.com>
2025-10-17 20:07:29 +0500

    cms connected

--- src/collections/Questions.ts
+++ src/collections/Questions.ts
@@ -1,84 +1,89 @@
-import { CollectionConfig } from "payload";
-
+import { CollectionConfig } from 'payload'
 
 export const Questions: CollectionConfig = {
-    slug: 'questions',
-    access: {
-        read: () => true,
+  slug: 'questions',
+  access: {
+    read: () => true,
+  },
+
+  fields: [
+    {
+      name: 'question_id',
+      type: 'text',
     },
-   
-    fields: [
-        {
-            name: 'question_id',
-            type: 'text',
-        },
-        {
-            name: 'exam_instance_id',
-            type: 'text',
-        },
-        {
-            name: 'stage_id',
-            type: 'text',
-        },
-        {
-            name: 'label_id',
-            type: 'text',
-        },
-        {
-            name: 'section_id',
-            type: 'text',
-        },
-        {
-            name: 'question_number',
-            type: 'text',
-        },
-        {
-            name: 'question_text',
-            type: 'text',
-        },
-        {
-            name: 'question_image_url',
-            type: 'text',
-        },
-        {
-            name: 'option_1_text',
-            type: 'text',
-        },
-        {
-            name: 'option_1_image_url',
-            type: 'text',
-        },
-        {
-            name: 'option_2_text',
-            type: 'text',
-        },
-        {
-            name: 'option_2_image_url',
-            type: 'text',
-        },
-        {
-            name: 'option_3_text',
-            type: 'text',
-        },
-        {
-            name: 'option_3_image_url',
-            type: 'text',
-        },
-        {
-            name: 'option_4_text',
-            type: 'text',
-        },
-        {
-            name: 'option_4_image_url',
-            type: 'text',
-        },
-        {
-            name: 'correct_option',
-            type: 'number',
-        },
-        {
-            name: 'explanation',
-            type: 'number',
-        },
-    ],
+    {
+      name: 'exam_instance_id',
+      type: 'text',
+    },
+    {
+      name: 'stage_id',
+      type: 'text',
+    },
+    {
+      name: 'label_id',
+      type: 'text',
+    },
+    {
+      name: 'section_id',
+      type: 'text',
+    },
+    {
+      name: 'question_number',
+      type: 'text',
+    },
+    {
+      name: 'question_text',
+      type: 'text',
+      localized: true,
+    },
+    {
+      name: 'question_image_url',
+      type: 'text',
+    },
+    {
+      name: 'option_1_text',
+      type: 'text',
+      localized: true,
+    },
+    {
+      name: 'option_1_image_url',
+      type: 'text',
+    },
+    {
+      name: 'option_2_text',
+      type: 'text',
+      localized: true,
+    },
+    {
+      name: 'option_2_image_url',
+      type: 'text',
+    },
+    {
+      name: 'option_3_text',
+      type: 'text',
+      localized: true,
+    },
+    {
+      name: 'option_3_image_url',
+      type: 'text',
+    },
+    {
+      name: 'option_4_text',
+      type: 'text',
+      localized: true,
+    },
+    {
+      name: 'option_4_image_url',
+      type: 'text',
+    },
+    {
+      name: 'correct_option',
+      type: 'number',
+    },
+    {
+      name: 'explanation',
+      type: 'textarea',
+      localized: true,
+    },
+  ],
 }
--- src/payload-migrations/index.ts
+++ src/payload-migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20251017_062920 from './20251017_062920';
 import * as migration_20251017_063721 from './20251017_063721';
 import * as migration_20251017_145756 from './20251017_145756';
+import * as migration_20251017_150723 from './20251017_150723';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20251017_145756.up,
     down: migration_20251017_145756.down,
-    name: '20251017_145756'
+    name: '20251017_145756',
+  },
+  {
+    up: migration_20251017_150723.up,
+    down: migration_20251017_150723.down,
+    name: '20251017_150723'
   },
 ];
