80f4523fee30a405f91a7739a7520f955a71f8c7
MuhammadArslan9300 <muhammadarslan9300@gmail.com>
2025-10-17 08:50:08 +0500

    cms connected

--- src/payload-migrations/index.ts
+++ src/payload-migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251017_034215 from './20251017_034215';
+import * as migration_20251017_035000 from './20251017_035000';
 
 export const migrations = [
   {
     up: migration_20251017_034215.up,
     down: migration_20251017_034215.down,
-    name: '20251017_034215'
+    name: '20251017_034215',
+  },
+  {
+    up: migration_20251017_035000.up,
+    down: migration_20251017_035000.down,
+    name: '20251017_035000'
   },
 ];
