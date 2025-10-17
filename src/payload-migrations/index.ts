1d69502a3ecad60ea175ffe96242a7532da616d5
MuhammadArslan9300 <muhammadarslan9300@gmail.com>
2025-10-17 08:42:36 +0500

    cms connected

---
+++ src/payload-migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251017_034215 from './20251017_034215';
+
+export const migrations = [
+  {
+    up: migration_20251017_034215.up,
+    down: migration_20251017_034215.down,
+    name: '20251017_034215'
+  },
+];
