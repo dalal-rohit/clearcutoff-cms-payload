75cd849799d9b71352aeb046f164c55fa6a5fba3
MuhammadArslan9300 <muhammadarslan9300@gmail.com>
2025-10-29 15:34:10 +0500

    new changes

--- src/payload-migrations/index.ts
+++ src/payload-migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20251024_134601 from './20251024_134601';
-import * as migration_20251029_101944 from './20251029_101944';
+import * as migration_20251029_103348 from './20251029_103348';
 
 export const migrations = [
   {
-    up: migration_20251024_134601.up,
-    down: migration_20251024_134601.down,
-    name: '20251024_134601',
-  },
-  {
-    up: migration_20251029_101944.up,
-    down: migration_20251029_101944.down,
-    name: '20251029_101944'
+    up: migration_20251029_103348.up,
+    down: migration_20251029_103348.down,
+    name: '20251029_103348'
   },
 ];
