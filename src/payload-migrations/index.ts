ea08f3eb9b1c1b7823fcf39c02fd0a6e109cb698
MuhammadArslan9300 <muhammadarslan9300@gmail.com>
2025-10-29 15:46:13 +0500

    new changes

--- src/payload-migrations/index.ts
+++ src/payload-migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251029_103348 from './20251029_103348';
+import * as migration_20251029_104551 from './20251029_104551';
 
 export const migrations = [
   {
     up: migration_20251029_103348.up,
     down: migration_20251029_103348.down,
-    name: '20251029_103348'
+    name: '20251029_103348',
+  },
+  {
+    up: migration_20251029_104551.up,
+    down: migration_20251029_104551.down,
+    name: '20251029_104551'
   },
 ];
